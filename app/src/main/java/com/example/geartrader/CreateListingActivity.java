8319e2fa796f53a011d4917d52313d00e9ccfbdb
marcuss <marcuss@coventry.ac.uk>
2021-03-03 23:02:34 +0000

    version 0.0.2
    Feat:
    - Added an empty activity for registering as a user
    - Added an empty activity for creating a new item listing
    - Changed center text view of each activity to match activity name

---
+++ app/src/main/java/com/example/geartrader/CreateListingActivity.java
@@ -0,0 +1,14 @@
+package com.example.geartrader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateListingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_listing);
+    }
+}
