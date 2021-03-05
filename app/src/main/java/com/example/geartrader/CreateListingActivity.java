b6d144fc973195ba1b960a168871fcd1c9ef6861
marcuss <marcuss@coventry.ac.uk>
2021-03-05 17:48:28 +0000

    version 0.0.8
    Feat:
    - Changed the layout of the other activities to relative from constraint
    - Added another button to main activity page to navigate to the Create Listing Activity
    - Added button to Register Activity to return to the main activity
    - Added button to Create Listing Activity to return to the main activity

--- app/src/main/java/com/example/geartrader/CreateListingActivity.java
+++ app/src/main/java/com/example/geartrader/CreateListingActivity.java
@@ -2,13 +2,30 @@ package com.example.geartrader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateListingActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_listing);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMain();
+            }
+        });
     }
+
+    public void openMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/geartrader/MainActivity.java
+++ app/src/main/java/com/example/geartrader/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +24,23 @@ public class MainActivity extends AppCompatActivity {
                 openRegister();
             }
         });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCreateListing();
+            }
+        });
     }
 
     public void openRegister() {
         Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCreateListing() {
+        Intent intent = new Intent(this, CreateListingActivity.class);
         startActivity(intent);
     }
 
